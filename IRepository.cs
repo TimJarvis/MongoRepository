94a448b553a420b829df0b5193e935083a31a219
Tim Jarvis <tim.jarvis@lonsec.com.au>
2020-03-04 16:56:33 +1100

    Added container name as an override / option and converted to net standard

--- IRepository.cs
+++ IRepository.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace MongoRepository
 {
     public interface IRepository
     {
-        Task DeleteAsync<T, TKey>(Expression<Func<T, bool>> expression) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task DeleteAsync<T, TKey>(TKey id) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task DeleteAll<T, TKey>() where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task<T> SingleAsync<T, TKey>(Expression<Func<T, bool>> expression) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task<T> SingleAsync<T, TKey>(TKey id) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task<(int total, IList<T> results)> ListAsync<T, TKey>(int skip = 0, int take = 50) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task<(int total, IList<T> results)> ListWithFilterAsync<T, TKey>(Expression<Func<T,bool>> filter, int skip = 0, int take = 50) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task AddAsync<T, TKey>(T item) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
-        Task AddAsync<T, TKey>(IEnumerable<T> items) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task DeleteAsync<T, TKey>(Expression<Func<T, bool>> expression, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task DeleteAsync<T, TKey>(TKey id, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task DeleteAll<T, TKey>(string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task<T> SingleAsync<T, TKey>(Expression<Func<T, bool>> expression, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task<T> SingleAsync<T, TKey>(TKey id, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task<(int total, IList<T> results)> ListAsync<T, TKey>(int skip = 0, int take = 50, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task<(int total, IList<T> results)> ListAsync<T, TKey>( string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task<(int total, IList<T> results)> ListWithFilterAsync<T, TKey>(Expression<Func<T,bool>> filter, int skip = 0, int take = 50, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task<(int total, IList<T> results)> ListWithFilterAsync<T, TKey>(Expression<Func<T,bool>> filter, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task AddAsync<T, TKey>(T item, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
+        Task AddAsync<T, TKey>(IEnumerable<T> items, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>;
     }
 }
--- MongoDbRepository.cs
+++ MongoDbRepository.cs
@@ -18,69 +18,88 @@ namespace MongoRepository
             _db = db;
         }
 
-        public async Task DeleteAsync<T, TKey>(Expression<Func<T, bool>> expression) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+        public async Task DeleteAsync<T, TKey>(Expression<Func<T, bool>> expression, string collectionName = null) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
         {
-            var collection = _db.GetCollection<T>(typeof(T).Name);
+            var derivedCollectionName = typeof(T).Name;
+            var collection = _db.GetCollection<T>(collectionName ?? derivedCollectionName);
             await collection.DeleteManyAsync(expression);
         }
 
-        public async Task DeleteAsync<T, TKey>(TKey id) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+        public async Task DeleteAsync<T, TKey>(TKey id, string collectionName = null) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
         {
-            var collection = _db.GetCollection<T>(typeof(T).Name);
+            var derivedCollectionName = typeof(T).Name;
+            var collection = _db.GetCollection<T>(collectionName ?? derivedCollectionName);
             var filter = Builders<T>.Filter.Eq(doc => doc.Id, id);
             await collection.DeleteOneAsync(filter);
         }
 
-        public async Task DeleteAll<T, TKey>() where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+        public async Task DeleteAll<T, TKey>(string collectionName = null) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
         {
-            await _db.DropCollectionAsync(typeof(T).Name);
+            var derivedCollectionName = typeof(T).Name;
+            await _db.DropCollectionAsync(collectionName ?? derivedCollectionName);
         }
 
-        public async Task<T> SingleAsync<T, TKey>(Expression<Func<T, bool>> expression) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+        public async Task<T> SingleAsync<T, TKey>(Expression<Func<T, bool>> expression, string collectionName = null) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
         {
-            var collection = _db.GetCollection<T>(typeof(T).Name);
+            var derivedCollectionName = typeof(T).Name;
+            var collection = _db.GetCollection<T>(collectionName ?? derivedCollectionName);
             return await collection.AsQueryable().Where(expression).SingleAsync();
         }
 
-        public async Task<(int total, IList<T> results)> ListAsync<T, TKey>(int skip = 0, int take = 50) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+        public async Task<(int total, IList<T> results)> ListAsync<T, TKey>(int skip = 0, int take = 50, string collectionName = null) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
         {
-            var query = _db.GetCollection<T>(typeof(T).Name).AsQueryable();
+            var derivedCollectionName = typeof(T).Name;
+            var query = _db.GetCollection<T>(collectionName ?? derivedCollectionName).AsQueryable();
             var total = query.CountAsync();
             var results = query.Skip(skip).Take(take).ToListAsync();
             await Task.WhenAll(total, results);
             return (total.Result, (IList<T>)results.Result);
         }
-        
-        public async Task<(int total, IList<T> results)> ListWithFilterAsync<T, TKey>(Expression<Func<T, bool>> filter, int skip = 0, int take = 50) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+
+        public Task<(int total, IList<T> results)> ListAsync<T, TKey>(string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>
         {
-            var query = _db.GetCollection<T>(typeof(T).Name).AsQueryable();
+            return ListAsync<T,TKey>(0, 50, collectionName);
+        }
+
+        public async Task<(int total, IList<T> results)> ListWithFilterAsync<T, TKey>(Expression<Func<T, bool>> filter, int skip = 0, int take = 50, string collectionName = null) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+        {
+            var derivedCollectionName = typeof(T).Name;
+            var query = _db.GetCollection<T>(collectionName ?? derivedCollectionName).AsQueryable();
             query = query.Where(filter);
             var total = query.CountAsync();
             var results = query.Skip(skip).Take(take).ToListAsync();
             await Task.WhenAll(total, results);
             return (total.Result, (IList<T>)results.Result);
         }
-        
-        public async Task<T> SingleAsync<T, TKey>(TKey id) where T : IDocument<TKey>  where TKey : IEquatable<TKey> 
+
+        public  Task<(int total, IList<T> results)> ListWithFilterAsync<T, TKey>(Expression<Func<T, bool>> filter, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>
         {
-            var collection = _db.GetCollection<T>(typeof(T).Name);
+            return ListWithFilterAsync<T, TKey>(filter, 0, 50, collectionName);
+        }
+        
+        public async Task<T> SingleAsync<T, TKey>(TKey id, string collectionName = null) where T : IDocument<TKey>  where TKey : IEquatable<TKey>
+        {
+            var derivedCollectionName = typeof(T).Name;
+            var collection = _db.GetCollection<T>(collectionName ?? derivedCollectionName);
             var filter = Builders<T>.Filter.Eq(doc => doc.Id, id);
             return await collection.Find(filter).SingleAsync();
         }
 
-        public async Task AddAsync<T, TKey>(T item) where T : IDocument<TKey> where TKey : IEquatable<TKey>
+        public async Task AddAsync<T, TKey>(T item, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>
         {
+            var derivedCollectionName = typeof(T).Name;
             var def = default(TKey);
             if (item.Id == null || (def != null && def.Equals(item.Id)))
             {
                 throw new Exception("You must provide a key to store this document.");
             }
-            var collection = _db.GetCollection<T>(typeof(T).Name);
+            var collection = _db.GetCollection<T>(collectionName ?? derivedCollectionName);
             await collection.InsertOneAsync(item);
         }
 
-        public async Task AddAsync<T, TKey>(IEnumerable<T> items) where T : IDocument<TKey> where TKey : IEquatable<TKey>
+        public async Task AddAsync<T, TKey>(IEnumerable<T> items, string collectionName = null) where T : IDocument<TKey> where TKey : IEquatable<TKey>
         {
+            var derivedCollectionName = typeof(T).Name;
             foreach (var item in items)
             {
                 var def = default(TKey);
@@ -90,7 +109,7 @@ namespace MongoRepository
                 }
             }
 
-            var collection = _db.GetCollection<T>(typeof(T).Name);
+            var collection = _db.GetCollection<T>(collectionName ?? derivedCollectionName);
             await collection.InsertManyAsync(items);
         }
         
